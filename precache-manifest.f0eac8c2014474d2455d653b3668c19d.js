3c9a4a3729d204350fb576bfc4a1febf400e8f22
mdgarden <chooi9522@gmail.com>
2020-10-25 08:53:08 +0900

    Updates

---
+++ precache-manifest.f0eac8c2014474d2455d653b3668c19d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10aed9bcb69d86c50e35fc6c2534bbe1",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "417c6671994855eb54d9",
+    "url": "/movie_app_2020/static/css/main.888d1aad.chunk.css"
+  },
+  {
+    "revision": "56b49c2a4b8b295d9f5b",
+    "url": "/movie_app_2020/static/js/2.db1e8dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.db1e8dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417c6671994855eb54d9",
+    "url": "/movie_app_2020/static/js/main.4c7006f5.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
