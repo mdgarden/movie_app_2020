de865da05ce6cd0695509b47ca7241b333642509
mdgarden <chooi9522@gmail.com>
2020-10-26 02:00:17 +0900

    #6.4 Redirecting fin

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function Detail(props) {
-  console.log(props);
-  return <span>hello</span>;
+class Detail extends React.Component {
+  componentDidMount() {
+    console.log(this.props);
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
